4d6cc9137bbea3ce38d631cce8c8aac79805eeee
EC2 Default User <ec2-user@ip-172-31-30-226.ap-northeast-1.compute.internal>
2021-11-04 08:58:50 +0000

    135. Answerモデルの作成

--- rails_projects/qanda/db/schema.rb
+++ rails_projects/qanda/db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211029015714) do
+ActiveRecord::Schema.define(version: 2021_11_04_085340) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
+    t.string "name"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
