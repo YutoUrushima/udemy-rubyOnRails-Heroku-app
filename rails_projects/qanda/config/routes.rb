a317d86da88bff6e732bf4553a20dd17f99e5f1a
EC2 Default User <ec2-user@ip-172-31-30-226.ap-northeast-1.compute.internal>
2021-11-04 06:18:40 +0000

    118. 新規質問の投稿 View

--- rails_projects/qanda/config/routes.rb
+++ rails_projects/qanda/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
+  
   root 'questions#index'
   resources :questions
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
 end
